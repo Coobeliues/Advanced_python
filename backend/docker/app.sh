0a7baa314a837ead82d81711f8c3f95381791b84
Aitolkyn <aitolkynamantai17@gmail.com>
2023-12-11 18:45:43 +0600

    telegbot and backend are working together

--- backend/docker/app.sh
+++ backend/docker/app.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-alembic revision --autogenerate -m "Reconciliation and daily amount tables"
+alembic revision --autogenerate -m "Reconciliation and daily amount tables" &
 alembic upgrade head
+
+sleep 5 
+
+
 cd modules
-python3 teleg.py
+python3 teleg.py &
 cd ..
+
+
 python3 main.py
-# gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+
