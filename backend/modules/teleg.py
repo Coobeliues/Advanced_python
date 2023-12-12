c7588d6000396f63d7eb7ab16f5ad0ee4e640bd8
Aitolkyn <aitolkynamantai17@gmail.com>
2023-12-12 16:23:16 +0600

    kafka is half done

--- backend/modules/teleg.py
+++ backend/modules/teleg.py
@@ -5,6 +5,8 @@ import modules.model as _model
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from sqlalchemy import select
+import modules.userServices as _userServices
+
 
 async def connect_to_db():
     await _dbase.DB.connect()
@@ -36,21 +38,22 @@ async def get_data_command(message: types.Message):
 async def get_data(message: types.Message):
     user_id = message.from_user.id
     username = message.text
-    await save_username(username, user_id)
-    await message.answer(f'Thank you, {username}! Your username has been saved.')
+    res = await save_username(username, user_id)
+    if not res :
+        await message.answer(f'Thank you, {username}! Your username has been saved.')
+    else : 
+        await message.answer(f'{username}, wait for the results.')
 
     query = select([_model.request2.c.bin]).where(_model.request2.c.username == username)
     result = await _dbase.DB.fetch_one(query)
     
     if result is not None:
         bin_iin = result[0] 
-        lang = 'en'
-        data = get_datas(bin_iin, lang)
-
-        print(f"API Response: {data}")
+        # lang = 'en'
+        data = await _userServices.pdf_generation_consumer(bin_iin)
 
         if 'success' in data and data['success']:
-            generate_pdf(data)
+            # _userServices.generate_pdf(data)
             with open(PDF_PATH, 'rb') as pdf_file:
                 await bot.send_document(message.chat.id, pdf_file)
             await message.answer('Here is your PDF')
@@ -64,70 +67,61 @@ async def save_username(username, user_id):
     existing_user = await _dbase.DB.fetch_one(query)
 
     if existing_user:
-        await bot.send_message(user_id, "Username already exists. Please try another one.")
-        return
+        return False 
+    else:
+        query1 = _model.telegram_users.insert().values(username=username, chat_id=str(user_id))
+        await _dbase.DB.execute(query1)
+        return True
 
-    query1 = _model.telegram_users.insert().values(username=username, chat_id=str(user_id))
-    await _dbase.DB.execute(query1)
 
-def get_datas(bin_iin, lang='en'):
-    api_url = f'https://old.stat.gov.kz/api/juridical/counter/api/?bin={bin_iin}&lang={lang}'
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error during API request: {e}")
-        return {}
+# def generate_pdf(data):
+#     pdf = FPDF()
+#     pdf.add_page()
+#     pdf.add_font('DejaVuSans', '', '/appp/DejaVuSans.ttf', uni=True)
+#     pdf.set_font("DejaVuSans", size=12)
 
-def generate_pdf(data):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.add_font('DejaVuSans', '', '/appp/DejaVuSans.ttf', uni=True)
-    pdf.set_font("DejaVuSans", size=12)
-
-    bin_iin_text = f"BIN/IIN: {data['obj']['bin']}"
-    name_text = f"Name: {data['obj']['name']}"
-    register_date_text = f"Registration Date: {data['obj']['registerDate']}"
-    oked_code_text = f"Main code of the GCoEA: {data['obj']['okedCode']}"
-    oked_name_text = f"Type of Economic Activity: {data['obj']['okedName']}"
-    secondOkeds_text = f"Secondary code of the GCoEA: {data['obj']['secondOkeds']}"
-    krpCode_text = f"Code of CoDE: {data['obj']['secondOkeds']}"
-    krpName_text = f"Name of CoDE: {data['obj']['krpName']}"
-    krpBfCode_text = f"Code of CoDE (excluding branches): {data['obj']['krpBfCode']}"
-    krpBfName_text = f"Name of CoDE: {data['obj']['krpBfName']}"
-    kseCode_text = f"CoATO: {data['obj']['kseCode']}"
-    kseName_text = f"Name of the economic sector: {data['obj']['kseName']}"
-    kfsCode_text = f"KFP code: {data['obj']['kfsCode']}"
-    kfsName_text = f"KFP name: {data['obj']['katoId']}"
-    katoCode_text = f"CoATO: {data['obj']['katoCode']}"
-    katoId_text = f"CoATO Id: {data['obj']['katoId']}"
-    katoAddress_text = f"Legal address: {data['obj']['katoAddress']}"
-    fio_text = f"Surname, name, patronymic of the head: {data['obj']['fio']}"
+#     bin_iin_text = f"BIN/IIN: {data['obj']['bin']}"
+#     name_text = f"Name: {data['obj']['name']}"
+#     register_date_text = f"Registration Date: {data['obj']['registerDate']}"
+#     oked_code_text = f"Main code of the GCoEA: {data['obj']['okedCode']}"
+#     oked_name_text = f"Type of Economic Activity: {data['obj']['okedName']}"
+#     secondOkeds_text = f"Secondary code of the GCoEA: {data['obj']['secondOkeds']}"
+#     krpCode_text = f"Code of CoDE: {data['obj']['secondOkeds']}"
+#     krpName_text = f"Name of CoDE: {data['obj']['krpName']}"
+#     krpBfCode_text = f"Code of CoDE (excluding branches): {data['obj']['krpBfCode']}"
+#     krpBfName_text = f"Name of CoDE: {data['obj']['krpBfName']}"
+#     kseCode_text = f"CoATO: {data['obj']['kseCode']}"
+#     kseName_text = f"Name of the economic sector: {data['obj']['kseName']}"
+#     kfsCode_text = f"KFP code: {data['obj']['kfsCode']}"
+#     kfsName_text = f"KFP name: {data['obj']['katoId']}"
+#     katoCode_text = f"CoATO: {data['obj']['katoCode']}"
+#     katoId_text = f"CoATO Id: {data['obj']['katoId']}"
+#     katoAddress_text = f"Legal address: {data['obj']['katoAddress']}"
+#     fio_text = f"Surname, name, patronymic of the head: {data['obj']['fio']}"
 
 
    
-    pdf.multi_cell(0, 10, txt=bin_iin_text)
-    pdf.multi_cell(0, 10, txt=name_text)
-    pdf.multi_cell(0, 10, txt=register_date_text)
-    pdf.multi_cell(0, 10, txt=oked_code_text)
-    pdf.multi_cell(0, 10, txt=oked_name_text)
-    pdf.multi_cell(0, 10, txt=secondOkeds_text)
-    pdf.multi_cell(0, 10, txt=krpCode_text)
-    pdf.multi_cell(0, 10, txt=krpName_text)
-    pdf.multi_cell(0, 10, txt=krpBfCode_text)
-    pdf.multi_cell(0, 10, txt=krpBfName_text)
-    pdf.multi_cell(0, 10, txt=kseCode_text)
-    pdf.multi_cell(0, 10, txt=kseName_text)
-    pdf.multi_cell(0, 10, txt=kfsCode_text)
-    pdf.multi_cell(0, 10, txt=kfsName_text)
-    pdf.multi_cell(0, 10, txt=katoCode_text)
-    pdf.multi_cell(0, 10, txt=katoId_text)
-    pdf.multi_cell(0, 10, txt=katoAddress_text)
-    pdf.multi_cell(0, 10, txt=fio_text)
+#     pdf.multi_cell(0, 10, txt=bin_iin_text)
+#     pdf.multi_cell(0, 10, txt=name_text)
+#     pdf.multi_cell(0, 10, txt=register_date_text)
+#     pdf.multi_cell(0, 10, txt=oked_code_text)
+#     pdf.multi_cell(0, 10, txt=oked_name_text)
+#     pdf.multi_cell(0, 10, txt=secondOkeds_text)
+#     pdf.multi_cell(0, 10, txt=krpCode_text)
+#     pdf.multi_cell(0, 10, txt=krpName_text)
+#     pdf.multi_cell(0, 10, txt=krpBfCode_text)
+#     pdf.multi_cell(0, 10, txt=krpBfName_text)
+#     pdf.multi_cell(0, 10, txt=kseCode_text)
+#     pdf.multi_cell(0, 10, txt=kseName_text)
+#     pdf.multi_cell(0, 10, txt=kfsCode_text)
+#     pdf.multi_cell(0, 10, txt=kfsName_text)
+#     pdf.multi_cell(0, 10, txt=katoCode_text)
+#     pdf.multi_cell(0, 10, txt=katoId_text)
+#     pdf.multi_cell(0, 10, txt=katoAddress_text)
+#     pdf.multi_cell(0, 10, txt=fio_text)
 
-    pdf.output(PDF_PATH)
-    print(data)
+#     pdf.output(PDF_PATH)
+#     print(data)
 
 async def main():
     await connect_to_db()
--- backend/modules/userServices.py
+++ backend/modules/userServices.py
@@ -3,14 +3,89 @@ import modules.model as _model
 from dbase import DB
 from modules.services import check_is_done, get_user_information
 import requests
-router = APIRouter()
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
+import json
+from fpdf import FPDF
+import asyncio
 
-@router.post("/editInfo")
-@check_is_done()
-async def edit_personal_info(user: _model.UserRead, user1: _model.UserRead = Depends(get_user_information)):
-    user_data = user.dict()
-    query = _model.requests.insert().values(user_id=user1.user_id, is_done=False, confirmed = False, type = "Editing personal info", datas_from_users=user_data)
-    result = await DB.execute(query)
+router = APIRouter()
+KAFKA_BOOTSTRAP_SERVERS = 'broker:9094'
+KAFKA_TOPIC_1 = 'topic1'
+KAFKA_TOPIC_2 = 'topic2'
+async def produce_to_kafka(data):
+    producer_conf = {
+        'bootstrap_servers': KAFKA_BOOTSTRAP_SERVERS
+    }
+    producer = AIOKafkaProducer(**producer_conf)
+    
+    await producer.start()
+    await producer.send_and_wait(KAFKA_TOPIC_1, key=None, value=data)
+    await producer.stop()
+
+PDF_PATH = '/appp/output.pdf'
+
+async def bin_info_consumer():
+    consumer = AIOKafkaConsumer(KAFKA_TOPIC_1, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS, group_id='bin_info_group')
+    await consumer.start()
+    
+    async for message in consumer:
+        bin_info_request = json.loads(message.value)
+        parsed_data = process_bin_info(bin_info_request)
+        
+        producer = AIOKafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+        await producer.start()
+        await producer.send_and_wait(KAFKA_TOPIC_2, value=json.dumps(parsed_data).encode('utf-8'))
+        await producer.stop()
+
+async def continuous_bin_info_consumer():
+    while True:
+        await bin_info_consumer()
+        await asyncio.sleep(10)
+
+async def pdf_generation_consumer(bin_iin):
+    consumer = AIOKafkaConsumer(KAFKA_TOPIC_2, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS, group_id='pdf_generation_group')
+    await consumer.start()
+    
+    async for message in consumer:
+        parsed_data = json.loads(message.value)
+        if parsed_data.get("bin") == bin_iin:
+            generate_pdf(parsed_data)
+            return parsed_data
+ 
+
+def process_bin_info(bin_info_request):
+    bin_data = get_datas(bin_info_request["bin"])
+    return bin_data 
+
+def get_datas(bin_iin, lang='en'):
+    api_url = f'https://old.stat.gov.kz/api/juridical/counter/api/?bin={bin_iin}&lang={lang}'
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error during API request: {e}")
+        return {}
+    
+def generate_pdf(data):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    if 'bin' in data:
+        for key, value in data.items():
+            pdf.cell(0, 10, f"{key}: {value}", ln=True)
+        pdf.output(PDF_PATH)
+    else:
+        print("Key 'bin' not found in data")
+
+
+
+# @router.post("/editInfo")
+# @check_is_done()
+# async def edit_personal_info(user: _model.UserRead, user1: _model.UserRead = Depends(get_user_information)):
+#     user_data = user.dict()
+#     query = _model.requests.insert().values(user_id=user1.user_id, is_done=False, confirmed = False, type = "Editing personal info", datas_from_users=user_data)
+#     result = await DB.execute(query)
   
 def check_company_by_bin(bin: str, lang: str):
     url = f"https://old.stat.gov.kz/api/juridical/counter/api/?bin={bin}&lang={lang}"
@@ -27,6 +102,13 @@ def check_company_by_bin(bin: str, lang: str):
         print(e)
         return {"exists": False, "Name": ""}
 
+@router.post("/editInfo")
+@check_is_done()
+async def edit_personal_info(user: _model.UserRead, user1: _model.UserRead = Depends(get_user_information)):
+    user_data = user.dict()
+    query = _model.requests.insert().values(user_id=user1.user_id, is_done=False, confirmed=False, type="Editing personal info", datas_from_users=user_data)
+    result = await DB.execute(query)
+
 @router.get("/check-company/{bin}/{lang}")
 async def check_company(bin: str, lang: str):
     result = check_company_by_bin(bin, lang)
@@ -36,4 +118,8 @@ async def check_company(bin: str, lang: str):
 async def check_company(request: _model.Request2Read): 
     query = _model.request2.insert().values(username=request.username, bin=request.bin, result=request.result)
     await DB.execute(query)
-
+    data = json.dumps({"bin": request.bin}).encode('utf-8')
+    
+    await produce_to_kafka(data)
+    
+    asyncio.create_task(continuous_bin_info_consumer())
